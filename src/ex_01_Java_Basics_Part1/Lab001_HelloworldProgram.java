533e06755dfba8f796dae3cf060cd66002597525
SoumyashreeGitAcc <kulkarnisoumya19@gmail.com>
2025-05-17 15:12:05 +0530

    This is first code

--- src/ex_01_Java_Basics_Part1/Lab001_HelloworldProgram.java
+++
@@ -1,4 +0,0 @@
-package ex_01_Java_Basics_Part1;
-
-public class Lab001_HelloworldProgram {
-}
---
+++ src/ex_01_Java_Basics_Part1/Lab_001_Helloworld.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics_Part1;
+
+public class Lab_001_Helloworld {
+    public static void main(String[] args) {
+        System.out.println("This is Helloworld Program");
+    }
+}
